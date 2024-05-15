cc307646d2afdd59c20393daf5b422a76e485058
gmattedi <giulio.mattedi@gmail.com>
2024-05-15 14:05:42 +0100

    add neighbor tests

---
+++ tests/data/README.md
@@ -0,0 +1,4 @@
+Files:
+
+- [`inputs1.smi`](inputs1.smi): 10 molecules
+- [`inputs2.smi`](inputs2.smi): 15 molecules, of which the last 5 are the first 5 of `inputs1.smi`, so to have some overlap
---
+++ tests/test_neighbors.py
@@ -0,0 +1,39 @@
+import os
+
+import numpy as np
+import pytest
+
+from chemicalspace.layers.neigbors import ChemicalSpaceNeighborsLayer
+
+np.random.seed(42)
+
+TESTS_DIR = os.path.dirname(os.path.abspath(__file__))
+
+INPUT_SMI_FILES = [
+    os.path.join(TESTS_DIR, "data", name) for name in ["inputs1.smi", "inputs2.smi"]
+]
+
+
+@pytest.fixture
+def space() -> ChemicalSpaceNeighborsLayer:
+    return ChemicalSpaceNeighborsLayer.from_smi(INPUT_SMI_FILES[0])
+
+
+@pytest.fixture
+def other_space() -> ChemicalSpaceNeighborsLayer:
+    return ChemicalSpaceNeighborsLayer.from_smi(INPUT_SMI_FILES[1])
+
+
+def test_overlap(space, other_space, radius=0.4, min_neighbors=1):
+    indices = space.find_overlap(
+        other_space, radius=radius, min_neighbors=min_neighbors
+    )
+    assert isinstance(indices, np.ndarray)
+    assert len(indices) == 5
+
+
+def test_carve(space, other_space, radius=0.4, min_neighbors=1):
+    other_space_carved = other_space.carve(
+        space, radius=radius, min_neighbors=min_neighbors
+    )
+    assert len(other_space - other_space_carved) == 5
